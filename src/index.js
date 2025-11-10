2d44e2d0a38d34da2846bfa7e50359ec3660f84c
Raphael811 <raphaelpeters811@gmail.com>
2025-11-10 10:05:48 +0100

    Fix import statements for App and index.css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';  // <-- must match your App.jsx file
-import './index.css';          // optional
+import App from './App.jsx';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
