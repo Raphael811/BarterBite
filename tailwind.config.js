c66b9dfdda262303d8277e4bcbb89dbea4288db6
Raphael811 <raphaelpeters811@gmail.com>
2025-11-14 10:30:33 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // Specify all files that should be scanned for Tailwind CSS classes
   content: [
-    "./src//*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
     "./public/index.html",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
